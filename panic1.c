cb0c62f75ef872d3b6e1e7ba48658f4e824b81f4
Amandeep Kaur <51168375+Amandeepkaur786@users.noreply.github.com>
2020-07-17 10:04:24 -0400

    coding of interfacing panic button with LPC2148

--- panic1.c
+++ panic1.c
@@ -9,23 +9,24 @@ int delay (int time)
 	}
 }
 void main()
-{
-	PINSEL0=0x00000000;
-	PINSEL2=0x00000000;
+{	
 	
-	IODIR0=0x00000001;
+	PINSEL0=0x00000000; //make P0.0 to P0.15 as a GPIO pins
+	PINSEL2=0x00000000;  //make P1.16 to P0.31 as a GPIO pins
+	
+	IODIR0=0x00000001;     //Set P0.0 as a output
 	
 	while(1)
 	{
-		if(input&(1<<16))
+		if(input&(1<<16))    //input is provided by P0.16
 		{
 			
-		IOSET0=(1<<0);
+		IOSET0=(1<<0);   //value of P0.0 is set
 		delay(1000);
 		}
 	else 
 		{
-		IOCLR0=(1<<0);
+		IOCLR0=(1<<0);  //value of P0.0 is clear
 		delay(1000);
 }
 }
