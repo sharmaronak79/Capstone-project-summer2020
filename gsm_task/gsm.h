461120cb26fdebf8659529fb4068a529f46df79e
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-07-21 13:55:03 -0400

    Add files via upload

---
+++ gsm_task/gsm.h
@@ -0,0 +1,75 @@
+void ser_init(void);
+void tx(unsigned char c);
+unsigned char rx(void);
+void tx_str(unsigned char *s);
+ 
+void sms(unsigned char *num1,unsigned char *msg);
+void gsm_delay(void);
+ 
+unsigned int dell;
+
+
+
+
+
+
+void sms(unsigned char *num1,unsigned char *msg)
+{
+    tx_str("AT");
+    tx(0x0d);
+    gsm_delay();
+ 
+    tx_str("AT+CMGF=1");
+    tx(0x0d);
+    gsm_delay();
+ 
+    tx_str("AT+CMGS=");
+    tx('"');
+    while(*num1)
+        tx(*num1++);
+    tx('"');
+    tx(0x0d);
+    gsm_delay();
+ 
+    while(*msg)
+        tx(*msg++);
+    tx(0x1a);
+    gsm_delay();
+}
+ 
+void gsm_delay()
+{
+    unsigned long int gsm_del,ff;
+    for(gsm_del=0;gsm_del<=500000;gsm_del++)
+         for(ff=0;ff<25;ff++);
+}
+ 
+void ser_init()
+{
+    VPBDIV=0x02;      //PCLK = 30MHz
+    PINSEL0=0x5;
+    U0LCR=0x83;
+    U0DLL=195;
+    U0DLM=0;
+    U0LCR=0x03;
+    U0TER=(1<<7);
+}
+ 
+void tx(unsigned char c)
+{
+    U0THR=c;
+    while((U0LSR&(1<<5))==0);
+}
+ 
+void tx_str(unsigned char *s)
+{
+    while(*s) {
+        tx(*s++);
+    }
+}
+ 
+unsigned char rx()
+{
+    while((U0LSR&(1<<0))==0);
+    return U0RBR;
+}
---
+++ gsm_task/gsm_embt.d
@@ -0,0 +1,3 @@
+.\gsm_embt.o: gsm_embt.c
+.\gsm_embt.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
+.\gsm_embt.o: gsm.h
